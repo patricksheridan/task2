001f3e9ff0df16bc5ad7e3eec5de35fa5f2f0a6a
Neil Browning <n.browning@kainos.com>
2014-08-14 14:33:21 +0100

    project added to ignore

--- src/employee_stuff/Main.java
+++ src/employee_stuff/Main.java
@@ -1,23 +1,49 @@
 package employee_stuff;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Class driver = Class.forName("com.mysql.jdbc.Driver");
-		Connection c = 
-		      DriverManager.getConnection("jdbc:mysql://localhost/task2", 
-		            "root", "ch@ngeme1");
-		Statement st = c.createStatement();
-		ResultSet rs = st.executeQuery(
-		      "SELECT idEmployee, FName FROM Employee");
-		while(rs.next()){
-		   String out = String.format("%s is in %s.", 
-		   rs.getString("idEmployee"), rs.getString("FName"));
-		   System.out.println(out);
-		}
-
+	public static void main(String[] args) {
+		outputMenu();
 	}
 
+	private static void outputMenu() {
+		System.out.println("Choose a menu option: ");
+		System.out.println("1. List all employees");
+		Scanner myScanner = new Scanner(System.in);
+		int menuOption = myScanner.nextInt();
+		if (menuOption == 1) {
+			listAllEmployees();
+		}
+	}
+
+	private static void listAllEmployees() {
+		try {
+			Class driver = Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e1) {
+			System.out.println(e1.getMessage());
+		}
+		try {
+			Connection c = 
+					DriverManager.getConnection("jdbc:mysql://localhost/task2", 
+		            "root", "ch@ngeme1");
+			Statement st = c.createStatement();
+			ResultSet rs = st.executeQuery(
+					"SELECT idEmployee, FName, SName, Salary FROM Employee");
+			while(rs.next()){
+				String out = String.format("Employee %s: %s %s has a salary of %s.", 
+						rs.getString("idEmployee"), rs.getString("FName"), rs.getString("SName"),
+						rs.getString("Salary"));
+				System.out.println(out);
+			}
+		} catch(SQLException e2) {
+			System.out.println(e2.getMessage());
+		}
+	}
 }
